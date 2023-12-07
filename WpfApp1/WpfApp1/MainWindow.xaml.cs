94470d64c4ac1bffc822145275052d57ba1398f4
ImDaeseong <cs93059@gmail.com>
2023-12-07 16:55:37 +0900

    FirebaseAdmin_test

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using FirebaseAdmin.Messaging;
+using FirebaseAdmin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Google.Apis.Auth.OAuth2;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string token = "d9gUEmE_Tt2Y24QeRAT4QA:APA91bERXuM6IH-a_NLRa6Vb52KzKnMxCuy1nbvppEtTJytkRWz3_TXx_y_WxXiXyyIEf6YPn-Q8JvvVJjsQt9U8XjOjGGbr741PSSs79_Y4-VMk-4J8WJc1dCsZf3VXaKZqpb76k5Tl";
+        private FirebaseApp firebaseApp = null;
+        private FirebaseMessaging firebaseMessaging = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            init();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await SendFCMMessage(token, "전달할 제목", "전달할 내용 입니다");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+
+        private void init()
+        {
+            firebaseApp = FirebaseApp.Create(new AppOptions()
+            {
+                Credential = GoogleCredential.FromFile("E:\\FirebaseAdmin_test\\winfrmFCM\\serviceAccountKey.json"),
+            });
+            firebaseMessaging = FirebaseMessaging.GetMessaging(firebaseApp);
+        }
+
+        private Task SendFCMMessage(string sToken, string sTitle, string sMsg)
+        {
+            var messageData = new Dictionary<string, string>()
+            {
+                { "title", sTitle },
+                { "message", sMsg }
+            };
+
+            var message = new FirebaseAdmin.Messaging.Message()
+            {
+                Data = messageData,
+                Token = sToken
+            };
+
+            return firebaseMessaging.SendAsync(message);
+        }
+
+    }
+}
--- winfrmFCM/winfrmFCM/Form1.cs
+++ winfrmFCM/winfrmFCM/Form1.cs
@@ -1,36 +1,17 @@
 ﻿using FirebaseAdmin;
-using FirebaseAdmin.Auth;
 using FirebaseAdmin.Messaging;
 using Google.Apis.Auth.OAuth2;
-using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using FirebaseAdmin;
-using FirebaseAdmin.Messaging;
-using Google.Apis.Auth.OAuth2;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using FirebaseAdmin.Messaging;
-using System.Net;
-using System.Runtime.Remoting.Messaging;
 
 namespace winfrmFCM
 {
     public partial class Form1 : Form
     {
-
         private string token = "d9gUEmE_Tt2Y24QeRAT4QA:APA91bERXuM6IH-a_NLRa6Vb52KzKnMxCuy1nbvppEtTJytkRWz3_TXx_y_WxXiXyyIEf6YPn-Q8JvvVJjsQt9U8XjOjGGbr741PSSs79_Y4-VMk-4J8WJc1dCsZf3VXaKZqpb76k5Tl";
-
-        private FirebaseApp FirebaseApp = null;
+        private FirebaseApp firebaseApp = null;
         private FirebaseMessaging firebaseMessaging = null;
 
         public Form1()
@@ -45,21 +26,27 @@ namespace winfrmFCM
 
         private async void button1_Click(object sender, EventArgs e)
         {
-            await sendFCMMessage(token, "전달할 제목", "전달할 내용 입니다");
+            try
+            {
+                await SendFCMMessage(token, "전달할 제목", "전달할 내용 입니다");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
         }
 
         private void init()
         {
-            FirebaseApp = FirebaseApp.Create(new AppOptions()
+            firebaseApp = FirebaseApp.Create(new AppOptions()
             {
                 Credential = GoogleCredential.FromFile("E:\\FirebaseAdmin_test\\winfrmFCM\\serviceAccountKey.json"),
             });
-            firebaseMessaging = FirebaseMessaging.GetMessaging(FirebaseApp);
+            firebaseMessaging = FirebaseMessaging.GetMessaging(firebaseApp);
         }
 
-        private Task sendFCMMessage(string sToken, string sTitle, string sMsg)
+        private Task SendFCMMessage(string sToken, string sTitle, string sMsg)
         {
-            var client_token = sToken;
             var messageData = new Dictionary<string, string>()
             {
                 { "title", sTitle },
@@ -72,8 +59,7 @@ namespace winfrmFCM
                 Token = sToken
             };
 
-            return FirebaseMessaging.DefaultInstance.SendAsync(message);
+            return firebaseMessaging.SendAsync(message);
         }
-
     }
 }
